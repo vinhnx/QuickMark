44d3369a5398b8f421ff1c50b348b07bc776c02c
vinhnx <vinhnguyen2308@gmail.com>
2013-04-25 19:49:04 +0700

    css

--- NXVMarkdown/ViewController.m
+++ NXVMarkdown/ViewController.m
@@ -17,24 +17,47 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+
+
+    NSError *error;
+
+    NSString *markdown = @"# Example\nWhat a *library*! **this** is a [link](http://google.com/)\nAnd cat :3\n\n![cat](http://placekitten.com/g/290/250)\n\nSee *log* for ouput html";
+
+    NSMutableString *html = [[NSMutableString alloc] init];
+    [html appendString:[NSString stringWithFormat:@"<html>"
+                        "<head>"
+                        "  <meta charset='UTF-8'/>"
+                        "  <style>%@</style>"
+                        "</head>", [self css]]];
     
-    
-    NSError  *error;
-    NSString *markdown   = @"# Example\nWhat a *library*! **this** is a [link](http://google.com/)\nAnd cat :3\n\n![cat](http://placekitten.com/200/300)\n\nSee *log* for ouput html";
-    
-    NSString *html = [MMMarkdown HTMLStringWithMarkdown:markdown error:&error];
-    
+    [html appendString:[MMMarkdown HTMLStringWithMarkdown:markdown error:&error]];
+
     [_markdownView loadHTMLString:html baseURL:nil];
-    
+
     NSLog(@"%@", html);
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (NSString *)css {
+    NSError *error = nil;
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"style" ofType:@"css"];
+    NSString *cssString = [NSString stringWithContentsOfFile:filePath
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:&error];
+
+    if (error != nil) {
+        NSLog(@"Error: %@", error);
+        
+        return nil;
+    }
+
+    return cssString;
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
